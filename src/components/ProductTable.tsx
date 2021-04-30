bd97374fcc201321ec424f2b3efdcf60ad007584
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-04-30 09:36:26 +0600

    make product table responsive

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -3,21 +3,23 @@ import Product from "../models/Product";
 
 export class ProductTable extends Component<{ products: Product[], callbackfn: (product: Product, index: number) => JSX.Element }> {
     render() {
-        return <table className={"my-2 table table-bordered"}>
-            <thead>
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Price</th>
-                <th scope="col">Category</th>
-                <th scope="col">Description</th>
-                <th scope="col">Image</th>
-                <th scope="col" className={'text-center'}><span className={"material-icons"}>settings</span></th>
-            </tr>
-            </thead>
-            <tbody>{
-                this.props.products.map(this.props.callbackfn)
-            }
-            </tbody>
-        </table>;
+        return <div className={'my-2 table-responsive'}>
+            <table className={"table table-bordered"}>
+                <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Image</th>
+                    <th scope="col" className={'text-center'}><span className={"material-icons"}>settings</span></th>
+                </tr>
+                </thead>
+                <tbody>{
+                    this.props.products.map(this.props.callbackfn)
+                }
+                </tbody>
+            </table>
+        </div>;
     }
 }
